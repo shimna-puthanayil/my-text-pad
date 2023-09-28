ae8fa30f8399ddc3fb64889d502f2ae331329940
shimna <chimmu.p@gmail.com>
2023-09-28 16:08:31 +1000

    added workbox plugins for service worker and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,69 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
+  const injectManifest = new InjectManifest({
+    swSrc: "./src-sw.js",
+    swDest: "src-sw.js",
+    ...(argv.mode !== "production" ? { exclude: [/./] } : {}),
+  });
+
+  if (argv.mode !== "production") {
+    // In dev, suppress the "InjectManifest has been called multiple times" warning by reaching into
+    // the private properties of the plugin and making sure it never ends up in the state
+    // where it makes that warning.
+    Object.defineProperty(injectManifest, "alreadyCalled", {
+      get() {
+        return false;
+      },
+      set() {
+        // do nothing; the internals try to set it to true, which then results in a warning
+        // on the next run of webpack.
+      },
+    });
+  }
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({ template: "./index.html", title: "Text Editor" }),
+      // Injects our custom service worker
+      injectManifest,
 
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Text Editor",
+        short_name: "TE",
+        description: "Edit your text here!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
     module: {
-      rules: [
-        
-      ],
+      rules: [],
     },
   };
 };
